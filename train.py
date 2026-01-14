0d866ff0af04aebd4eea77c383c153ace31cc5cf
anthonyzhou-1 <anthonyzhou@berkeley.edu>
2026-01-14 15:14:36 -0500

    readme

--- train.py
+++ train.py
@@ -93,7 +93,8 @@ def main(args):
                         callbacks=[checkpoint_callback, lr_monitor, EMAWeightAveraging(trainconfig["ema_decay"])],
                         logger=wandb_logger,
                         num_sanity_val_steps=1,
-                        accumulate_grad_batches=trainconfig.get("accumulate_grad_batches", 1),)
+                        accumulate_grad_batches=trainconfig.get("accumulate_grad_batches", 1),
+                        num_sanity_val_steps=trainconfig.get("num_sanity_val_steps", 1),)
     
     if trainconfig["checkpoint"] is not None:
         trainer.fit(model=model,
