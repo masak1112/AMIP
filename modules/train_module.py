09cfecde4d320402fc90f218e10bdb7785ca293b
anthonyzhou-1 <anthonyzhou@berkeley.edu>
2026-01-15 14:18:45 -0500

    climatology

--- modules/train_module.py
+++ modules/train_module.py
@@ -146,6 +146,13 @@ class TrainModule(L.LightningModule):
                     self.diagnostic_state = batch['diagnostic']
                     self.invariant_state = batch['invariants']
 
+                    print(batch['surface'].shape)
+                    print(batch['multilevel'].shape)
+                    print(batch['diagnostic'].shape)
+                    print(batch['invariants'].shape)
+                    print(batch['forcing'].shape)
+                    print(batch['scalars'].shape)
+
                     self.surface_running_mean = self.n.denormalize_surface(self.surface_state)
                     self.multilevel_running_mean = self.n.denormalize_multilevel(self.multilevel_state)
                     self.diagnostic_running_mean = self.n.denormalize_diagnostic(self.diagnostic_state)
@@ -234,8 +241,9 @@ class TrainModule(L.LightningModule):
     def update(self, batch):
         # b = 1 
         # assume these are normalized
+
         forcing_input = batch['forcing'] # b nlat nlon c
-        scalar_input = batch['scalars'] # b t 2
+        scalar_input = batch['scalars'] # b 2
 
         surface_input = self.surface_state # b nlat nlon c
         multilevel_input = self.multilevel_state # b nlevel nlat nlon c
