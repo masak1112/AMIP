4849d569c16fd61b17186a56cd4d537e6f74b326
anthonyzhou-1 <anthonyzhou@berkeley.edu>
2026-01-15 15:43:51 -0500

    multigpu

--- debug.sh
+++ debug.sh
@@ -9,8 +9,19 @@
 # Enable GPU-MPI (if supported by application)
 export NCCL_DEBUG=INFO
 
-ml conda
-conda activate pt220gpu_conda
+ml ncarenv/24.12
+ml craype/2.7.31
+ml intel/2024.2.1
+ml ncarcompilers/1.0.0
+ml libfabric/1.15.2.0
+ml cuda/12.3.2
+ml cray-mpich/8.1.29
+ml hdf5/1.12.3
+ml netcdf/4.9.2
+ml conda/latest
+ml mkl/2024.2.2
+
+conda activate credit
 
 # MPI and OpenMP settings
 NNODES=`wc -l < $PBS_NODEFILE`
