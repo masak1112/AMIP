e0c4288359bdb88f58c2ed23891f534c4ec59c11
Anthony Zhou <ayz@derecho2.hsn.de.hpc.ucar.edu>
2026-01-16 09:08:30 -0700

    modules

--- load_modules.sh
+++ load_modules.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
+ml gcc/12.4.0
 ml ncarenv/24.12
 ml craype/2.7.31
-ml intel/2024.2.1
 ml ncarcompilers/1.0.0
 ml libfabric/1.15.2.0
 ml cuda/12.3.2
@@ -10,5 +10,5 @@ ml cray-mpich/8.1.29
 ml hdf5/1.12.3
 ml netcdf/4.9.2
 ml conda/latest
+ml intel/2024.2.1
 ml mkl/2024.2.2
-ml gcc/12.4.0
