8efe63a4dbb26a233672db4534467ebb4eff113a
anthonyzhou-1 <anthonyzhou@berkeley.edu>
2026-01-16 11:02:50 -0500

    multigpuf

---
+++ load_modules.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+ml ncarenv/24.12
+ml craype/2.7.31
+ml intel/2024.2.1
+ml ncarcompilers/1.0.0
+ml libfabric/1.15.2.0
+ml cuda/12.3.2
+ml cray-mpich/8.1.29
+ml hdf5/1.12.3
+ml netcdf/4.9.2
+ml conda/latest
+ml mkl/2024.2.2
+ml gcc/12.4.0
