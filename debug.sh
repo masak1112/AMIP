48b964cfee2b5e250574bcb9af406f1c6a725efb
anthonyzhou-1 <anthonyzhou@berkeley.edu>
2026-01-20 09:55:23 -0500

    ae

--- debug.sh
+++ debug.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -l
 #PBS -N debug
 #PBS -l select=1:ncpus=16:ngpus=4:mem=120G
-#PBS -q develop
-#PBS -l walltime=0:10:00
+#PBS -q main
+#PBS -l walltime=12:00:00
 #PBS -A UCHI0014
 #PBS -j oe
 
--- train.py
+++ train.py
@@ -69,6 +69,7 @@ def main(args):
     checkpoint_callback  = ModelCheckpoint(
         filename= "model_{epoch:02d}",
         dirpath=path,
+        save_top_k=5,
         every_n_train_steps = 1000, # also save every 1000 training steps,
         save_last=True
     )
