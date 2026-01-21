a051650da1cfcb3c5c198721bcb864a00873ed4d
anthonyzhou-1 <anthonyzhou@berkeley.edu>
2026-01-21 17:05:04 -0500

    flow

---
+++ main_flow.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -l
+#PBS -N AE_main
+#PBS -l select=1:ncpus=16:ngpus=4:mem=120G
+#PBS -q main
+#PBS -l walltime=12:00:00
+#PBS -A UCHI0014
+#PBS -j oe
+
+module load conda
+conda activate torch2
+
+CONFIG=configs/flow.yaml
+
+python train.py --config=$CONFIG
