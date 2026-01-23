4c23ce9115b418d5ea904cabe06c357c6524349b
anthonyzhou-1 <anthonyzhou@berkeley.edu>
2026-01-23 17:18:48 -0500

    flow

--- develop2.sh
+++ develop2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -l
-#PBS -N ae_develop
+#PBS -N ae_DC
 #PBS -l select=1:ncpus=16:ngpus=4:mem=120G
-#PBS -q develop
-#PBS -l walltime=0:30:00
+#PBS -q main
+#PBS -l walltime=12:00:00
 #PBS -A URIC0009
 #PBS -j oe
 
