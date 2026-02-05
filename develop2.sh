4ce5695c35f0fccee1296ad1bc1e3efe34ac74ca
anthonyzhou-1 <anthonyzhou@berkeley.edu>
2026-02-05 11:54:36 -0500

    atlas

--- develop2.sh
+++ develop2.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -l
-#PBS -N ae_DC
+#PBS -N ae_DC_diag
 #PBS -l select=1:ncpus=16:ngpus=4:mem=120G
-#PBS -q main
-#PBS -l walltime=12:00:00
+#PBS -q develop
+#PBS -l walltime=00:10:00
 #PBS -A URIC0009
 #PBS -j oe
 
 module load conda
 conda activate torch2
 
-CONFIG=configs/ae_simple_DC.yaml
+CONFIG=configs/ae_simple_DC_diag.yaml
 
 python train.py --config=$CONFIG
