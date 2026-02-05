cb6a91317c048292de2547066f31df0ebde7f545
anthonyzhou-1 <anthonyzhou@berkeley.edu>
2026-02-05 12:12:16 -0500

    atlas

--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -l
-#PBS -N AE_simple_3D
+#PBS -N AE_decoder_diag
 #PBS -l select=1:ncpus=16:ngpus=4:mem=120G
 #PBS -q main
 #PBS -l walltime=12:00:00
@@ -9,6 +9,6 @@
 module load conda
 conda activate torch
 
-CONFIG=configs/ae_3D.yaml
+CONFIG=configs/ae_decoder_only_diag.yaml
 
 python train.py --config=$CONFIG
--- main2.sh
+++ main2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -l
-#PBS -N atlas_autoencoder
+#PBS -N AE_DC_diag
 #PBS -l select=1:ncpus=16:ngpus=4:mem=120G
 #PBS -q main
 #PBS -l walltime=12:00:00
@@ -9,6 +9,6 @@
 module load conda
 conda activate torch
 
-CONFIG=configs/ae_atlas.yaml
+CONFIG=configs/ae_simple_DC_diag.yaml
 
 python train.py --config=$CONFIG
