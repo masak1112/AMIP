3cf504a43a9c431baeebbd5a781b355dc24f1247
anthonyzhou-1 <anthonyzhou@berkeley.edu>
2026-02-09 12:45:19 -0500

    fa

--- modules/models/AE_simple.py
+++ modules/models/AE_simple.py
@@ -688,7 +688,7 @@ class Decoder(nn.Module):
                  resamp_with_conv = True,
                  separate_embedders=False,
                  kernel_size=3,
-                 padding=1
+                 padding=1,
                  ):
         super().__init__()
         self.hidden_channels = hidden_channels
@@ -956,3 +956,221 @@ class Decoder3D(Decoder):
         z_diagnostic = rearrange(z_diagnostic, 'b c zlat zlon -> b zlat zlon c')
 
         return z_surface, z_multilevel,  z_diagnostic
+
+class DecoderHistory(nn.Module):
+    def __init__(self,
+                 out_channels, # output channel dim
+                 hidden_channels, # width of network
+                 z_channels, # input latent dim
+                 z_channels_2,
+                 ch_mult=(1,2,2), 
+                 num_res_blocks = 2,
+                 resolution = (180, 360), 
+                 attn_resolutions = [32], 
+                 dropout=0.0, 
+                 double_z=True, 
+                 tanh_out=False,
+                 dim=2,
+                 padding_mode='zeros',
+                 upsample_type = 'avg',
+                 use_attn=False,
+                 resamp_with_conv = True,
+                 separate_embedders=False,
+                 kernel_size=3,
+                 padding=1,
+                 num_out_blocks=0,
+                 ):
+        super().__init__()
+        self.hidden_channels = hidden_channels
+        self.out_channels = out_channels
+        self.num_resolutions = len(ch_mult)
+        self.num_res_blocks = num_res_blocks
+        self.resolution = resolution
+        self.tanh_out = tanh_out
+        attn_type = "vanilla"
+        self.dim = dim
+        self.nsurface = 6
+        self.ndiagnostic = 9
+        self.nlevels = 26
+
+        # compute in_ch_mult, block_in and curr_res at lowest res
+        block_in = self.hidden_channels*ch_mult[self.num_resolutions-1]
+
+        curr_res = resolution[-1] // 2**(self.num_resolutions-1)
+
+        # z to block_in
+        self.conv_in = conv_nd(dim,
+                                z_channels,
+                                block_in,
+                                kernel_size=kernel_size,
+                                stride=1,
+                                padding=padding,
+                                padding_mode=padding_mode)
+
+        # middle
+        self.mid = nn.Module()
+        self.mid.block_1 = ResnetBlock(in_channels=block_in,
+                                       out_channels=block_in,
+                                       dropout=dropout,
+                                       dim=dim,
+                                       padding_mode=padding_mode,
+                                       padding=padding,
+                                       kernel_size=kernel_size)
+        self.mid.block_2 = ResnetBlock(in_channels=block_in,
+                                       out_channels=block_in,
+                                       dropout=dropout,
+                                       dim=dim,
+                                       padding_mode=padding_mode,
+                                       kernel_size=kernel_size,
+                                       padding=padding,)
+
+        # upsampling
+        self.up = nn.ModuleList()
+        for i_level in reversed(range(self.num_resolutions)):
+            block = nn.ModuleList()
+            attn = nn.ModuleList()
+            block_out = self.hidden_channels*ch_mult[i_level]
+            for i_block in range(self.num_res_blocks+1):
+                block.append(ResnetBlock(in_channels=block_in,
+                                         out_channels=block_out,
+                                         dropout=dropout,
+                                         dim=dim,
+                                         padding_mode=padding_mode,
+                                         padding=padding,
+                                         kernel_size=kernel_size))
+                block_in = block_out
+                if curr_res in attn_resolutions:
+                    attn.append(make_attn(block_in, attn_type=attn_type, dim=dim))
+                    print(f"added attn at res {curr_res}")
+            up = nn.Module()
+            up.block = block
+            up.attn = attn
+            if i_level != 0:
+                if upsample_type == 'dc':
+                    up.upsample = DCUpsample(block_in, 
+                                            block_in)
+                else:
+                    up.upsample = Upsample(block_in, resamp_with_conv, dim=dim)
+                curr_res = curr_res * 2
+            self.up.insert(0, up) # prepend to get consistent order
+        
+        self.conv_in_history = conv_nd(dim,
+                        z_channels_2,
+                        block_in,
+                        kernel_size=kernel_size,
+                        stride=1,
+                        padding=padding,
+                        padding_mode=padding_mode)
+
+        self.out_blocks = nn.ModuleList()
+        for _ in num_out_blocks:
+            self.out_blocks.append(ResnetBlock(in_channels=block_in*2,
+                                                  out_channels=block_in*2,
+                                                  dropout=dropout,
+                                                  dim=dim,
+                                                  padding_mode=padding_mode,
+                                                  padding=padding,
+                                                  kernel_size=kernel_size,))
+
+        self.out_blocks.append(ResnetBlock(in_channels=block_in*2,
+                        out_channels=out_channels,
+                        dropout=dropout,
+                        dim=dim,
+                        padding=padding,
+                        padding_mode=padding_mode,
+                        kernel_size=kernel_size,))
+
+        self.conv_out = conv_nd(dim,
+                                out_channels,
+                                out_channels,
+                                kernel_size=kernel_size,
+                                stride=1,
+                                padding=padding,
+                                padding_mode=padding_mode,
+                                groups = out_channels)
+        
+        # Apply He Initialization
+        self.apply(self._init_weights)
+
+    def _init_weights(self, m):
+        """
+        Applies He (Kaiming) initialization to Conv2d and Linear layers.
+        Initializes normalization layers (LayerNorm, BatchNorm) with scale 1 and bias 0.
+        """
+        if isinstance(m, (nn.Conv2d, nn.Conv3d, nn.Linear)):
+            nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            if m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        
+        elif isinstance(m, (nn.LayerNorm, nn.BatchNorm2d, nn.GroupNorm)):
+            if m.weight is not None:
+                nn.init.constant_(m.weight, 1)
+            if m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+
+    def assemble_input(self, surface, multilevel, diagnostic):
+        multilevel = rearrange(
+            multilevel, "b l h w c -> b h w (l c)"
+        )
+        out = torch.cat((surface, diagnostic, multilevel), dim=-1) # b h w c
+        out = rearrange(
+            out, "b h w c -> b c h w"
+        )
+
+        return out
+    
+    def disassemble_input(self, x):
+        x = rearrange(
+            x, "b c h w -> b h w c"
+        )
+
+        surface = x[..., : self.nsurface]
+        diagnostic = x[..., self.nsurface : self.nsurface + self.ndiagnostic]
+        multilevel = x[..., self.nsurface + self.ndiagnostic :]
+
+        multilevel = rearrange(
+            multilevel,
+            "b h w (l c) -> b l h w c",
+            l=self.nlevels,
+        )
+
+        return surface, multilevel, diagnostic
+
+    def forward(self, surface_history, multilevel_history, diagnostic_history,
+                z_surface, z_history, z_diagnostic) -> torch.Tensor:
+
+        # surface in shape b nlat nlon c 
+        # multilevel in shape b nlevel nlat nlon c
+        # diagnostic in shape b nlat nlon c
+
+        z = self.assemble_input(z_surface, z_history, z_diagnostic) # b c zlat zlon
+        history = self.assemble_input(surface_history, multilevel_history, diagnostic_history) # b c nlat nlon
+
+        # z to block_in
+        h = self.conv_in(z)
+        history = self.conv_in_history(history) 
+
+        # middle
+        h = self.mid.block_1(h)
+        h = self.mid.block_2(h)
+
+        # upsampling
+        for i_level in reversed(range(self.num_resolutions)):
+            for i_block in range(self.num_res_blocks+1):
+                h = self.up[i_level].block[i_block](h)
+                if len(self.up[i_level].attn) > 0:
+                    h = self.up[i_level].attn[i_block](h)
+            if i_level != 0:
+                h = self.up[i_level].upsample(h)
+
+        
+        h = torch.cat([h, history], dim=1)
+
+        for _ in self.out_blocks:
+            h = self.out_blocks(h)
+
+        h = self.conv_out(h)
+
+        surface_out, multilevel_out, diagnostic_out = self.disassemble_input(h)
+
+        return surface_out, multilevel_out, diagnostic_out
