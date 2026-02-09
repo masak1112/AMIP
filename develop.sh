7d8bf93fff4a1fdd9a2dd47a8e392b66b1f670a4
anthonyzhou-1 <anthonyzhou@berkeley.edu>
2026-02-09 14:37:35 -0500

    fix

--- develop.sh
+++ develop.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -l
-#PBS -N ae_history
+#PBS -N AE_Decoder_History2
 #PBS -l select=1:ncpus=16:ngpus=4:mem=120G
 #PBS -q develop
-#PBS -l walltime=0:10:00
+#PBS -l walltime=00:10:00
 #PBS -A URIC0009
 #PBS -j oe
 
 module load conda
 conda activate torch
 
-CONFIG=configs/ae_decoder_history.yaml
+CONFIG=configs/ae_decoder_history2_weighted.yaml
 
 python train.py --config=$CONFIG
