26c2f9b0a56bbffa5ffbc700be75690dc40fcf3d
anthonyzhou-1 <anthonyzhou@berkeley.edu>
2026-02-09 13:32:42 -0500

    history

--- develop2.sh
+++ develop2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -l
-#PBS -N ae_DC_diag
+#PBS -N ae_decoder_history_weighted
 #PBS -l select=1:ncpus=16:ngpus=4:mem=120G
 #PBS -q develop
 #PBS -l walltime=00:10:00
@@ -9,6 +9,6 @@
 module load conda
 conda activate torch
 
-CONFIG=configs/ae_simple_DC_diag.yaml
+CONFIG=configs/ae_decoder_history_weighted.yaml
 
 python train.py --config=$CONFIG
