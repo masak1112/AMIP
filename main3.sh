1f548b3b5e2aa20dbf35df8d370d2c5d410bf051
anthonyzhou-1 <anthonyzhou@berkeley.edu>
2026-02-10 16:20:52 -0500

    spectral

---
+++ main3.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -l
+#PBS -N AE_Decoder_History2_Spectral
+#PBS -l select=1:ncpus=16:ngpus=4:mem=120G
+#PBS -q main
+#PBS -l walltime=12:00:00
+#PBS -A UCHI0014
+#PBS -j oe
+
+module load conda
+conda activate torch
+
+CONFIG=configs/ae_decoder_history2_spectral.yaml
+
+python train.py --config=$CONFIG
