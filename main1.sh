d14c59398855e60a67a9c78f5690f456ec60eaf2
anthonyzhou-1 <anthonyzhou@berkeley.edu>
2026-02-17 09:46:06 -0500

    ddc

---
+++ main1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -l
+#PBS -N AE_CRPS_1
+#PBS -l select=1:ncpus=16:ngpus=4:mem=120G
+#PBS -q main
+#PBS -l walltime=12:00:00
+#PBS -A UCHI0014
+#PBS -j oe
+
+module load conda
+conda activate torch
+
+CONFIG=configs/ae_stochastic_1.yaml
+
+python train.py --config=$CONFIG
