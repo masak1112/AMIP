c2b139754302cd7eb3daebf7b7e0e5865a242d75
anthonyzhou-1 <anthonyzhou@berkeley.edu>
2026-02-16 20:58:38 -0500

    ddc

--- develop.sh
+++ develop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -l
-#PBS -N AE_SI_DDC
+#PBS -N AE_SI_DDC_history
 #PBS -l select=1:ncpus=16:ngpus=4:mem=120G
 #PBS -q develop
 #PBS -l walltime=00:30:00
@@ -9,6 +9,6 @@
 module load conda
 conda activate torch
 
-CONFIG=configs/ae_SI_DDC.yaml
+CONFIG=configs/ae_SI_DDC_history.yaml
 
 python train.py --config=$CONFIG
