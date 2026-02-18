cfb86b07e8a659c74e003568566d19ef980a05a1
anthonyzhou-1 <anthonyzhou@berkeley.edu>
2026-02-18 17:26:48 -0500

    ddc

--- develop.sh
+++ develop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -l
-#PBS -N AE_decoder_hfs
+#PBS -N AE_SI_DDC
 #PBS -l select=1:ncpus=16:ngpus=4:mem=120G
 #PBS -q develop
 #PBS -l walltime=00:10:00
@@ -9,6 +9,6 @@
 module load conda
 conda activate torch
 
-CONFIG=configs/ae_decoder_hfs_spectral.yaml
+CONFIG=configs/ae_SI_DDC.yaml
 
 python train.py --config=$CONFIG
