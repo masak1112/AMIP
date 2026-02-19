3641b398f178c17dce31b93f2c0f1d5a7d2f80b5
anthonyzhou-1 <anthonyzhou@berkeley.edu>
2026-02-19 16:42:40 -0500

    dit

--- mainSI.sh
+++ mainSI.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -l
-#PBS -N AE_flow
+#PBS -N SI
 #PBS -l select=1:ncpus=16:ngpus=4:mem=120G
 #PBS -q main
 #PBS -l walltime=12:00:00
@@ -9,6 +9,6 @@
 module load conda
 conda activate torch
 
-CONFIG=configs/ae_flow.yaml
+CONFIG=configs/SI.yaml
 
 python train.py --config=$CONFIG
