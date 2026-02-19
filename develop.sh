81914dc14df2a87c9801ba6e3316e86c9d962785
anthonyzhou-1 <anthonyzhou@berkeley.edu>
2026-02-19 11:12:50 -0500

    dit

--- develop.sh
+++ develop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -l
-#PBS -N AE_SI_DDC
+#PBS -N AE_Arches_DDC
 #PBS -l select=1:ncpus=16:ngpus=4:mem=120G
 #PBS -q develop
 #PBS -l walltime=00:10:00
@@ -9,6 +9,6 @@
 module load conda
 conda activate torch
 
-CONFIG=configs/ae_SI_DDC.yaml
+CONFIG=configs/ae_SI_arches.yaml
 
 python train.py --config=$CONFIG
