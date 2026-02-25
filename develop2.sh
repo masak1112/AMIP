66377a85584f064a41812ddfa66730891165640d
anthonyzhou-1 <anthonyzhou@berkeley.edu>
2026-02-25 16:46:24 -0500

    clouds

--- develop2.sh
+++ develop2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -l
-#PBS -N SI_DiT
+#PBS -N AE_Decoder_no_clouds
 #PBS -l select=1:ncpus=16:ngpus=4:mem=120G
 #PBS -q develop
 #PBS -l walltime=00:10:00
@@ -9,6 +9,6 @@
 module load conda
 conda activate torch
 
-CONFIG=configs/SI_DiT.yaml
+CONFIG=configs/ae_decoder_hfs_no_clouds.yaml
 
 python train.py --config=$CONFIG
