e0c37b9ecbd3404f2726b00a62cfe40532d95533
anthonyzhou-1 <anthonyzhou@berkeley.edu>
2026-02-25 19:50:15 -0500

    clouds

--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -l
-#PBS -N AE_HFS
+#PBS -N AE_DDC_no_clouds
 #PBS -l select=1:ncpus=16:ngpus=4:mem=120G
 #PBS -q main
 #PBS -l walltime=12:00:00
@@ -9,6 +9,6 @@
 module load conda
 conda activate torch
 
-CONFIG=configs/ae_decoder_hfs_spectral.yaml
+CONFIG=configs/ae_SI_DDC_no_clouds.yaml
 
 python train.py --config=$CONFIG
--- main1.sh
+++ main1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -l
-#PBS -N AE_HFS_FT
+#PBS -N AE_Decoder_no_clouds
 #PBS -l select=1:ncpus=16:ngpus=4:mem=120G
 #PBS -q main
 #PBS -l walltime=12:00:00
@@ -9,6 +9,6 @@
 module load conda
 conda activate torch
 
-CONFIG=configs/ae_decoder_hfs_ft.yaml
+CONFIG=configs/ae_decoder_hfs_no_clouds.yaml
 
 python train.py --config=$CONFIG
