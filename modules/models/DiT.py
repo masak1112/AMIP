7b1449a51a81529486825f410ffbf99fc6e69243
anthonyzhou-1 <anthonyzhou@berkeley.edu>
2026-03-09 12:21:57 -0400

    test

--- modules/models/DiT.py
+++ modules/models/DiT.py
@@ -79,7 +79,8 @@ class DiTBlock(nn.Module):
         q = apply_2d_rotary_pos_emb(q, rope_cos_lat, rope_sin_lat, rope_cos_lon, rope_sin_lon)
         k = apply_2d_rotary_pos_emb(k, rope_cos_lat, rope_sin_lat, rope_cos_lon, rope_sin_lon)
 
-        h = F.scaled_dot_product_attention(q, k, v).transpose(1, 2).reshape(b, n, c) # [b, n, dim]
+        h = F.scaled_dot_product_attention(q, k, v)
+        h = rearrange(h, "bs num_heads seqlen head_dim -> bs seqlen (num_heads head_dim)")
         h = self.attn_out(h) # [b, n, dim]
 
         x = x + gate_msa * h
