c402b54254559ed094fadbf12e2e5f84cb5f4b2f
anthonyzhou-1 <anthonyzhou@berkeley.edu>
2026-03-10 17:46:33 -0400

    test

---
+++ casper.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -l
+#PBS -N SI_Latent_DiT
+#PBS -l select=1:ncpus=4:ngpus=4:mem=120G:gpu_type=h100
+#PBS -q casper
+#PBS -l walltime=12:00:00
+#PBS -A UCHI0014
+#PBS -j oe
+
+module load conda
+conda activate torch
+
+CONFIG=configs/SI_Latent_DiT_med.yaml
+
+python train.py --config=$CONFIG
