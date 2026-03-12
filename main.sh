62fd66e2d618fce4132a5add106f1f72e347a98a
anthonyzhou-1 <anthonyzhou@berkeley.edu>
2026-03-12 11:28:44 -0400

    test

--- main.sh
+++ main.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 #PBS -N SI_Latent_DiT
-#PBS -l select=1:ncpus=16:ngpus=4:mem=120G
+#PBS -l select=1:ncpus=64:ngpus=4:mem=500G
 #PBS -q main
 #PBS -l walltime=12:00:00
 #PBS -A UCHI0014
@@ -9,6 +9,6 @@
 module load conda
 conda activate torch
 
-CONFIG=configs/SI_Latent_DiT_med.yaml
+CONFIG=configs/SI_Latent_DiT.yaml
 
 python train.py --config=$CONFIG
