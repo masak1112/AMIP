4a396fc25f4b0fc4d3512ca5c0bc8015cff9d064
anthonyzhou-1 <anthonyzhou@berkeley.edu>
2026-03-12 11:11:39 -0400

    test

--- develop.sh
+++ develop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 #PBS -N SI_Latent_DiT
-#PBS -l select=2:ncpus=8:ngpus=8:mem=120G
-#PBS -q preempt
+#PBS -l select=1:ncpus=16:ngpus=4:mem=240G
+#PBS -q develop
 #PBS -l walltime=00:05:00
 #PBS -A UCHI0014
 #PBS -j oe
