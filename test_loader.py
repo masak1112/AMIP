06dbdddeac59ce43350f567411e4d300a5f92940
anthonyzhou-1 <anthonyzhou@berkeley.edu>
2026-03-12 10:40:29 -0400

    test

--- test_loader.py
+++ test_loader.py
@@ -4,6 +4,7 @@ from tqdm import tqdm
 from datetime import timedelta
 from data.amip_new import get_out_path
 import os 
+import h5py
 
 config=get_yaml("configs/ae_SI_DDC.yaml")
 datamodule = ClimateDataModule(config['data'])
@@ -20,6 +21,10 @@ for i in tqdm(range(len(train_dataset))):
     data_idx = seconds_into_year // 3600 // train_dataset.data_timedelta_hours
     data_file_path = get_out_path(train_dataset.data_dir, data_year, data_idx)
 
-    if not os.path.exists(data_file_path):
-        print(f"Data file {data_file_path} does not exist.")
+    try:
+        f = h5py.File(data_file_path, 'r')
+        f.close()
+    except Exception as e:
+        print(f"Error opening data file {data_file_path}: {e}")
         break
+            
