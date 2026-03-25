3b4fe12433c747ee80182d14005fdcc0b9b6707f
anthonyzhou-1 <anthonyzhou@berkeley.edu>
2026-03-25 17:38:24 -0400

    crps

--- modules/models/Decoder.py
+++ modules/models/Decoder.py
@@ -358,8 +358,9 @@ class DecoderCNN(nn.Module):
                 self.out_scale.append(featscale2(patch_size=init_patch_size, channels=block_in))
 
         self.norm_out = Normalize(block_in) 
-        self.conv_out = ResnetBlock(in_channels=block_in,
-                                    out_channels=out_channels)
+        self.conv_out = SphereConv2d(in_channels=z_channels,
+                                    out_channels=block_in,
+                                    kernel_size=(3, 3), padding = (1, 1))
         
         # Apply He Initialization
         self.apply(self._init_weights)
