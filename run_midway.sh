3f05bb3d64f703c9a807bbe9141916ad0bb3fb9e
anthonyzhou-1 <anthonyzhou@berkeley.edu>
2026-04-03 12:02:39 -0400

    test

--- run_midway.sh
+++ run_midway.sh
@@ -18,6 +18,6 @@ conda activate /project/pedramh/ayz/envs/torch2
 export NCCL_P2P_DISABLE=1
 export NCCL_IB_DISABLE=1
 
-config_file=configs/EDM_midway.yaml
+config_file=configs/FM_midway.yaml
 
 srun python trainAE.py --config=$config_file
