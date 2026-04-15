85cfd2aafe7458f593c0fc2824970af04147c637
anthonyzhou-1 <ayz2@andrew.cmu.edu>
2026-04-15 14:57:40 -0600

    Test

--- run_midway.sh
+++ run_midway.sh
@@ -7,8 +7,8 @@
 #SBATCH --ntasks-per-node=4
 #SBATCH --gres=gpu:4
 #SBATCH --cpus-per-task=8 
-#SBATCH -o SI_%x_%j.out
-#SBATCH -e SI_%x_%j.err
+#SBATCH -o FM_%x_%j.out
+#SBATCH -e FM_%x_%j.err
 
 ml python
 conda activate /project/pedramh/ayz/envs/torch2
@@ -18,6 +18,6 @@ conda activate /project/pedramh/ayz/envs/torch2
 export NCCL_P2P_DISABLE=1
 export NCCL_IB_DISABLE=1
 
-config_file=configs/SI_midway.yaml
+config_file=configs/FM_midway.yaml
 
 srun python train.py --config=$config_file
