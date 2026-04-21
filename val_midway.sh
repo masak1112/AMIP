79c5f23b64052b444bf50a827fd6133840b5f857
anthonyzhou-1 <ayz2@andrew.cmu.edu>
2026-04-21 08:33:26 -0600

    test

--- val_midway.sh
+++ val_midway.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #SBATCH --account=pi-pedramh
-#SBATCH --time=2:00:00
+#SBATCH --time=2:30:00
 #SBATCH --mem=128G
 #SBATCH -p pedramh-gpu 
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=4 
-#SBATCH -o bias_%x_%j.out
-#SBATCH -e bias_%x_%j.err
+#SBATCH -o val_%x_%j.out
+#SBATCH -e val_%x_%j.err
 
 ml python
 conda activate /project/pedramh/ayz/envs/torch2
@@ -20,4 +20,4 @@ export NCCL_IB_DISABLE=1
 
 config_file=configs/combined_midway.yaml
 
-srun python bias.py --config=$config_file
+srun python val.py --config=$config_file
