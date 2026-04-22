32db229233c8a7abdc1facd8fc1d57ed8a999891
anthonyzhou-1 <ayz2@andrew.cmu.edu>
2026-04-22 09:30:26 -0600

    test

--- val_midway.sh
+++ val_midway.sh
@@ -18,6 +18,6 @@ conda activate /project/pedramh/ayz/envs/torch2
 export NCCL_P2P_DISABLE=1
 export NCCL_IB_DISABLE=1
 
-config_file=configs/SI_midway.yaml
+config_file=configs/combined_midway.yaml
 
 srun python val.py --config=$config_file
