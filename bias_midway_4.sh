966d7f5f83c26466edd48c0f434ba531b1931227
anthonyzhou-1 <ayz2@andrew.cmu.edu>
2026-04-28 07:10:54 -0600

    test

--- bias_midway_4.sh
+++ bias_midway_4.sh
@@ -20,4 +20,4 @@ export NCCL_IB_DISABLE=1
 
 config_file=configs/SI_midway.yaml
 
-srun python bias.py --config=$config_file --checkpoint="/project/pedramh/ayz/AMIP_logs/SI_X_forcings_42_2026-04-24T15-01-46/model_epoch=21.ckpt" --description="ep_21_forcing"
+srun python bias.py --config=$config_file --checkpoint="/project/pedramh/ayz/AMIP_logs/SI_X_forcings_42_2026-04-27T12-12-33/model_epoch=28.ckpt" --description="ep_28_forcing"
