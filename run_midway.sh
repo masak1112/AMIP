f396de33b7d334897ba4b9fadaeece6c6b5c50e7
anthonyzhou-1 <ayz2@andrew.cmu.edu>
2026-04-30 17:34:31 -0600

    test

--- run_midway.sh
+++ run_midway.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --account=pi-pedramh
-#SBATCH --time=72:00:00
-#SBATCH --mem=500G
+#SBATCH --time=48:00:00
+#SBATCH --mem=400G
 #SBATCH -p pedramh-gpu 
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=4
