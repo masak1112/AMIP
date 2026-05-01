f6936bc4254e5deac274fc9a8daf478cf8909f1c
anthonyzhou-1 <ayz2@andrew.cmu.edu>
2026-05-01 08:36:59 -0600

    Test

--- main.sh
+++ main.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -l
-#PBS -N xDDC
+#PBS -N SI
 #PBS -l select=1:ncpus=32:ngpus=4:mem=256G
 #PBS -q main
 #PBS -l walltime=12:00:00
-#PBS -A UCHI0014
+#PBS -A URIC0009
 #PBS -j oe
 
 module load conda
 conda activate torch
 
-CONFIG=configs/DDC_NCAR.yaml
+CONFIG=configs/SI_NCAR.yaml
 
 python train.py --config=$CONFIG
