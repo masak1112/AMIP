0fd38c155413393cb7777d9fe4b57e701d475822
Bing Gong <gongbing@uchicago.edu>
2026-07-20 08:13:44 -0600

    add develop_S2S.sh

--- develop_S2S.sh
+++ develop_S2S.sh
@@ -8,6 +8,10 @@
 source /glade/work/bgong/.venv/bin/activate
 
 #CONFIG=configs/DDC_NCAR.yaml
-CONFIG=configs/SI_NCAR_S2S.yaml
+#CONFIG=configs/SI_NCAR_S2S.yaml
+CONFIG=configs/SI_NCAR_S2S_crps.yaml
 
-python train.py --config=$CONFIG
+# Finetunes from /glade/work/bgong/amip/SI_checkpoints/last.ckpt (set via
+# training.partial_checkpoint in $CONFIG) with CRPS loss added on top of the
+# base interpolant loss. New checkpoints are saved under SI_checkpoints_crps.
+python train.py --config=$CONFIG --checkpoint=/glade/work/bgong/amip/SI_checkpoints_crps/last.ckpt
